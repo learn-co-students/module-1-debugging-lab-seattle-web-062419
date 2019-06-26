e065eef5b6262a95c52aa8205a0e8656a099f4e4
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-06-26 16:34:15 -0700

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,6 +1,7 @@
+require 'pry'
 class Person
   attr_accessor :name, :candy_lover
-
+  #binding.pry
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
@@ -19,11 +20,15 @@ class Person
   end
 
   def ballin_candy_lover?
+    #binding.pry
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
+      
     else
       10.times do
         "not a ballin candy lover"
+      end
     end
   end
+  
 end
